4f878f579fb4ad98ace1a6a857a802ca27dfceb9
ryanchrist <ryan.christ7@gmail.com>
2024-09-24 11:19:02 -0500

    Updating package descriptions.

--- R/QForm.R
+++ R/QForm.R
@@ -1,8 +1,15 @@
-#' QForm: A package for fast, safe screening with p-value bounds for quadratic forms.
+#' QForm: A package for fast, safe CDF/PDF estimation for generalized chi-square random varaibles and screening with p-value bounds for quadratic forms.
+#'
+#' QForm returns the CDF and PDF for the generalized chi-squared distribution with numerical accuracy deep into the tail (see \code{\link{QFGauss}}). It can also provide reliable upper and lower bounds on the CDF when only some of the chi-square terms are known (see \code{\link{QFGaussBounds}}).
+#' By using the fast Fourier transform in combination with novel concentration inequalities and various adjustments for numerical precision, QForm function is faster and more reliable than Davie's method and related approaches, especially when the returned CDF or PDF is to be evaluated at many points.
 #'
 #' Initially motivated by genome-wide association studies (GWAS), QForm is aimed at obtaining upper and lower bounds on p-values for test statistics with a limiting distribution of the form
-#' of \eqn{Q_f = T_f + R_f}.  Here \deqn{T_f = \sum\limits_{i \in \mathcal{T}} f\left(\eta_i \right) \left(Z_i + \delta_i\right)^2,}{T_f = \Sigma_{i \in T} f (\eta_i) (Z_i + \delta_i)^2,}
-#' \deqn{R_f = \sum\limits_{i \in \mathcal{R}} f\left(\eta_i \right) \left(Z_i + \delta_i\right)^2,}{R_f = \Sigma_{i \in R} f (\eta_i) (Z_i + \delta_i)^2,} where \eqn{Z_i \sim N(0,1)}{Z_i ~ N(0,1)}, and only the CDF of \eqn{T_f} is known.
+#' of \eqn{Q_f = T_f + R_f} where only the CDF of \eqn{T_f} is known. These random variables have the form
+#'
+#' \deqn{T_f = \sum\limits_{i \in \mathcal{T}} f\left(\eta_i \right) A_i + \sigma Z_0}{T_f = \Sigma_{i \in T} f (\eta_i) A_i + \sigma Z_0},
+#' \deqn{R_f = \sum\limits_{i \in \mathcal{R}} f\left(\eta_i \right) A_i}{T_f = \Sigma_{i \in R} f (\eta_i) A_i},
+#' where each \eqn{A_i \sim \chi^2_{a_i}\left(\delta^2_i\right)}{A_i \sim \chi^2_{a_i}\left(\delta^2_i\right)} and \eqn{Z_0 \sim N(0,1)}{Z_0 \sim N(0,1)}, all mututally independent,
+#' and \eqn{a_i = 1}{a_i = 1} for all \eqn{i \in \mathcal{R}}{i \in \mathcal{R}}. We aim to remove this final restriction in future work.
 #'
 #' In the genomics literature, SKAT and related methods have limiting distributions of this form.  In the machine learning and kernel methods literature, other popular test statistics share this limiting distribution, among them the Hilbert-Schimidt Information Criterion (HSIC).
 #' Approximate methods have emerged in the genomics (eg: FastSKAT) and kernel methods literature, have emerged based on the idea of using a top-k SVD to obtain \eqn{T_f} and then attempt to approximate the contribution from \eqn{R_f} using a single random variable that matches some of the moments of \eqn{R_f}.
